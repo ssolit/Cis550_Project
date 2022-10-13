0d13e43592e3ed489e5e2ba7b5cc90de36f96114
Spencer Solit <sjs@RSFAM-SJS-Pro22.local>
2022-10-13 10:36:54 -0400

    v1, visit  scroll page and collects 30 names

--- TheOrg_scraping/TO_junk_files/TO_scrollscrape.js
+++ TheOrg_scraping/TO_junk_files/TO_scrollscrape.js
@@ -1,62 +1,62 @@
-// starter code found here: https://www.scrapingbee.com/blog/infinite-scroll-puppeteer/
-
-
-// Puppeteer will not run without these lines
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
+const save_path = "./TO_comp_list_2.txt"
+
+// extract company names from urls 
 function extractItems() {
-/*  For extractedElements, you are selecting the tag and class,
-    that holds your desired information,
-    then choosing the desired child element you would like to scrape from.
-    in this case, you are selecting the
-    "<div class=blog-post />" from "<div class=container />" See below: */
-  const extractedElements = document.querySelectorAll('#container > div.blog-post');
-  const items = [];
-  for (let element of extractedElements) {
-    items.push(element.innerText);
-  }
-  return items;
-}
+    // set selector to JS path of relevent elements
+    let selector = '#__next > div.Layout_layoutWrapper__FFUAO > div > div > div > div.FilterResults_root__6CKVx > div > div > div > ul > li > div > div.ExploreCompanyRow_content___UlQH > ul > li.ExploreCompanyRow_seeMore__Onttj > a'
+                //    "#__next > div.Layout_layoutWrapper__FFUAO > div > div > div > div.FilterResults_root__6CKVx > div > div > div > ul > li > div > div.ExploreCompanyRow_content___UlQH > ul > li.ExploreCompanyRow_seeMore__Onttj > a"
+    const extractedElements = document.querySelectorAll(selector);
+      const names = [];
+      for (let element of extractedElements) {
+        let c_name = element.getAttribute('href').split("/")[2] // get name from url
+        names.push(c_name);
+      }
+      return names;
+    }
+
 
 async function scrapeItems(
-  page,
-  extractItems,
-  itemCount,
-  scrollDelay = 800,
-) {
-  let items = [];
-  try {
-    let previousHeight;
-    while (items.length < itemCount) {
-      items = await page.evaluate(extractItems);
-      previousHeight = await page.evaluate('document.body.scrollHeight');
-      await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
-      await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
-      await page.waitForTimeout(scrollDelay);
+    page,
+    extractItems,
+    itemCount,
+    scrollDelay = 800,
+    ) {
+    let items = [];
+    try {
+        let previousHeight;
+        while (items.length < itemCount) {
+            console.log("in scrapeItems. items.length = " + items.length);
+            items = await page.evaluate(extractItems);
+            previousHeight = await page.evaluate('document.body.scrollHeight');
+            await page.evaluate('window.scrollTo(0, document.body.scrollHeight)');
+            await page.waitForFunction(`document.body.scrollHeight > ${previousHeight}`);
+            await page.waitForTimeout(scrollDelay);
+        }
+    } catch(e) { }
+    return items;
     }
-  } catch(e) { }
-  return items;
-}
 
 (async () => {
-  // Set up Chromium browser and page.
-  const browser = await puppeteer.launch({
-    headless: false,
-    args: ['--no-sandbox', '--disable-setuid-sandbox'],
-  });
-  const page = await browser.newPage();
-  page.setViewport({ width: 1280, height: 926 });
-
-  // Navigate to the example page.
-  await page.goto('https://mmeurer00.github.io/infinite-scroll-example/');
-
-  // Auto-scroll and extract desired items from the page. Currently set to extract ten items.
-  const items = await scrapeItems(page, extractItems, 10);
-
-  // Save extracted items to a new file.
-  fs.writeFileSync('./items.txt', items.join('\n') + '\n');
-
-  // Close the browser.
-  await browser.close();
-})();
+    // Set up Chromium browser and page.
+    const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+    const page = await browser.newPage();
+    page.setViewport({ width: 1280, height: 926 });
+    
+    // Navigate to the example page.
+    await page.goto('https://theorg.com/organizations?countries=united-states');
+    
+    // Auto-scroll and extract desired items from the page. Currently set to extract ten items.
+    const items = await scrapeItems(page, extractItems, 10);
+    
+    // Save extracted items to a new file.
+    fs.writeFileSync(save_path, items.join('\n') + '\n');
+    
+    // Close the browser.
+    await browser.close();
+    })();
