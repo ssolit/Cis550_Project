892a2cc5597370989610c23193f3cfd55f9abac6
Spencer Solit <sjs@RSFAM-SJS-Pro22.local>
2022-10-13 20:47:45 -0400

    added logic to scrape a broader range of company sizes and stop after 100,000 entries. Doesn't make it past size 1-10 tho

--- TheOrg_scraping/scrape_TheOrg.js
+++ TheOrg_scraping/scrape_TheOrg.js
@@ -70,18 +70,48 @@ const fs = require('fs');
 
     }
 
-    
-    const comp_sizes = ['\"200-500\"', '\"50-200\"', '\"10-50\"'];
+    // const comp_sizes = ['\"200-500\"', '\"50-200\"', '\"10-50\"'];
+    const comp_sizes = ["\"1-10\",\"10-50\",\"50-200\",\"200-500\",\"500-1000\",\"1000-5000\",\"5000-10000\",\">10000\""];
     let industries = JSON.parse(fs.readFileSync("industries.json", 'utf-8'))["data"]["companyIndustries"];
 
-    await scrape_one_url(comp_sizes[0], industries[0]["id"]);
+    // await scrape_one_url(comp_sizes[0], industries[0]["id"]); // for debugging
 
-    // let total_count = 0;
-    // for (let i = 0; i < comp_sizes.length; i++) {
-    //     for (let j = 0; j < industries.length; j++) {
-    //         total_count += await scrape_one_url(comp_sizes[i], industries[i]["id"]);
-    //         console.log(`i = ${i}, j=${j}, total_count = ${total_count}`)
-    //     }
-    // }
+    let total_count = 0;
+    for (let i = 0; i < comp_sizes.length; i++) {
+        for (let j = 0; j < industries.length; j++) {
+            total_count += await scrape_one_url(comp_sizes[i], industries[i]["id"]);
+            console.log(`i = ${i}, j=${j}, total_count = ${total_count}`)
+            if (total_count > 100000) {
+                break;
+            }
+        }
+        if (total_count > 100000) {
+            break;
+        }
+    }
 
 })();
+
+
+
+
+fetch("https://prod-graphql-api.theorg.com/graphql", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/json",
+    "newrelic": "eyJ2IjpbMCwxXSwiZCI6eyJ0eSI6IkJyb3dzZXIiLCJhYyI6IjI1OTExNzYiLCJhcCI6IjExMzQyMTEzMTQiLCJpZCI6ImFmZWRlNzNhMzBjYTE1ZmMiLCJ0ciI6IjQ0MzgxNjhlNDM3OWZkNjc0NjYzMDhkMjIwNDVkZTg5IiwidGkiOjE2NjU3MDgxNjczNzh9fQ==",
+    "sec-ch-ua": "\"Chromium\";v=\"106\", \"Google Chrome\";v=\"106\", \"Not;A=Brand\";v=\"99\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "traceparent": "00-4438168e4379fd67466308d22045de89-afede73a30ca15fc-01",
+    "tracestate": "2591176@nr=0-1-2591176-1134211314-afede73a30ca15fc----1665708167378",
+    "Referer": "https://theorg.com/",
+    "Referrer-Policy": "strict-origin-when-cross-origin"
+  },
+  "body": "{\"operationName\":\"exploreCompanies\",\"variables\":{\"countries\":[],\"categories\":[],\"employeeRanges\":[\"1-10\",\"10-50\",\"50-200\",\"200-500\",\"500-1000\",\"1000-5000\",\"5000-10000\",\">10000\"],\"offset\":0,\"limit\":30},\"query\":\"query exploreCompanies($countries: [String!], $categories: [String!], $employeeRanges: [String!], $limit: Int!, $offset: Int!) {\\n  exploreCompanies(\\n    countries: $countries\\n    categories: $categories\\n    employeeRanges: $employeeRanges\\n    limit: $limit\\n    offset: $offset\\n  ) {\\n    paging {\\n      total\\n      pages\\n      current\\n      __typename\\n    }\\n    results {\\n      company {\\n        id\\n        slug\\n        name\\n        logoImage {\\n          uri\\n          ext\\n          versions\\n          endpoint\\n          prevailingColor\\n          placeholderDataUrl\\n          __typename\\n        }\\n        description\\n        positionCount\\n        verification {\\n          verificationType\\n          __typename\\n        }\\n        adminLocked\\n        __typename\\n      }\\n      following\\n      id\\n      positionExamples {\\n        slug\\n        image {\\n          uri\\n          ext\\n          versions\\n          endpoint\\n          prevailingColor\\n          placeholderDataUrl\\n          __typename\\n        }\\n        __typename\\n      }\\n      __typename\\n    }\\n    __typename\\n  }\\n}\\n\"}",
+  "method": "POST"
+});
